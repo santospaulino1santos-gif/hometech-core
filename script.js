9fadb10a3a8da687362bc8027a3970aacc3d1760
santospaulino1santos-gif <santospaulino1santos@gmail.com>
2026-06-29 09:13:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -204,8 +204,8 @@ async function addProduto() {
  COMPRAR
 ********************/
 function comprar(nome, preco) {
-  let numero = "258849042071";
-  let msg = `Olá, gostei do produto *${nome}* (${preco} MT) e quero comprar. Onde nos podemos encontrar?
+  let numero = "258868292000";
+  let msg = `Olá hometech, gostei do produto *${nome}* (${preco} MT) e quero comprar. Onde nos podemos encontrar?
   mensagem vinda do site hometech`;
   window.open("https://wa.me/" + numero + "?text=" + encodeURIComponent(msg), "_blank");
 }
